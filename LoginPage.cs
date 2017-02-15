3fd21775090aeb7fe53e62963a073e82f5e61d6c
Lica <Lica>
2017-02-15 21:40:09 +0000

    Nemanja | Dodao liniju

--- LoginPage.cs
+++ LoginPage.cs
@@ -13,6 +13,7 @@ namespace ConsoleApplication1
         {
             driver.Url = "http://store.demoqa.com/products-page/your-account/";
         }
+
         public IWebElement PasswordField { get { return driver.FindElement(By.Id("pwd")); } }
         public IWebElement UsernameField { get { return driver.FindElement(By.Id("log")); } }
         public void ClickLoginButton()
